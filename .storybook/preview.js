dd92a00f25afd926b44924317d92afb5ac74bebd
promethey <sedelkovegor@gmail.com>
2022-04-30 13:38:24 +0300

    Add bootstrap-icons

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap-icons/fonts/bootstrap-icons.css';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
