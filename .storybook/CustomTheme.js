13bb7c42eeffb177d8b3e3480031d11d2fe8b9c0
promethey <sedelkovegor@gmail.com>
2022-05-18 04:41:13 +0300

    Add custome theme for storybook

---
+++ .storybook/CustomTheme.js
@@ -0,0 +1,8 @@
+import {create} from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Bootstrap-UI',
+  brandUrl: 'https://github.com/promethey/bootstrap-ui-react',
+  brandImage: '../img/boostrap-ui-logo.png'
+})
