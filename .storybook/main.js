f8c88356d9d9507a777f2823bac573ec219db479
promethey <sedelkovegor@gmail.com>
2023-05-01 00:29:21 +0300

    Edit /stories folder

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = {
-  "stories": ["../src/stories/**/Introduction.stories.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
+  "stories": ["../src/stories/Introduction.stories.mdx", "../src/stories/*.stories.@(js|jsx|ts|tsx|mdx)"],
   "addons": ["@storybook/addon-links", "@storybook/addon-docs", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
   "framework": {
     name: "@storybook/react-webpack5",
