3506922d5f6cad7aa9cd55d0de56353cf4ddf96a
promethey <sedelkovegor@gmail.com>
2023-05-22 10:12:08 +0300

    Update StoryBook and Alert component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,20 @@
 const path = require('path');
 module.exports = {
-  "stories": ["../stories/Introduction.stories.mdx", "../stories/*.stories.@(js|jsx|ts|tsx|mdx)"],
+  "stories": [
+    "../stories/Introduction.stories.mdx",
+    "../stories/*.stories.mdx",
+    "../stories/*.stories.@(js|jsx|ts|tsx|mdx)"
+  ],
   "addons": ["@storybook/addon-links", "@storybook/addon-docs", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
   "framework": {
     name: "@storybook/react-webpack5",
     options: {}
   },
   webpackFinal: async (config) => {
-    config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, '../src')];
+    config.resolve.modules = [
+      ...(config.resolve.modules || []),
+      path.resolve(__dirname, '../src')
+    ];
     return config;
   },
   docs: {
