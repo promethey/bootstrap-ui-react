be504ff3cfd7fe5b01c02674bf20be8b03b1367d
promethey <sedelkovegor@gmail.com>
2023-05-23 22:14:56 +0300

    Edit storybook design

--- .storybook/CustomTheme.js
+++ .storybook/CustomTheme.js
@@ -1,7 +1,10 @@
-import {create} from '@storybook/theming';
+import { create } from '@storybook/theming';
+import brandImage from '../static/bootstrap-ui-low.png';
 
 export default create({
   base: 'light',
   brandTitle: 'Bootstrap UI',
   brandUrl: 'https://github.com/promethey/bootstrap-ui-react',
+  brandImage: brandImage,
+  fontBase: 'system-ui,-apple-system,"Segoe UI",Roboto,"Helvetica Neue","Noto Sans","Liberation Sans",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";',
 });
--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,14 @@
 import { addons } from '@storybook/addons';
 import CustomTheme from './CustomTheme';
+import favicon from '../static/favicon-32x32.png';
 
 addons.setConfig({
   theme: CustomTheme,
 });
+
+// change favicon in document
+const link = document.createElement('link');
+link.setAttribute('rel', 'shortcut icon');
+link.setAttribute('href', favicon);
+document.head.appendChild(link);
+
